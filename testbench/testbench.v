a84dca9c4c494700ec7cd3b91501f185c49bdbba
amanahuja62 <48840330+amanahuja62@users.noreply.github.com>
2020-04-19 17:08:18 +0530

    Update testbench.v

--- testbench/testbench.v
+++ testbench/testbench.v
@@ -25,9 +25,10 @@ module testbench;
 	Receiver uut2 (TXdataOut,samplingClock,reset,dataOut);
    
 	initial begin
-	reset=0; 
+	reset=0;  // active low asynchronous reset
 	#1 reset=1;
-	#18 TXStart=1; datain=8'b10010101; 
+	#18 TXStart=1; datain=8'b10010101; // TXStart makes the transmitter go the the start state. dataIn is the 
+		                           // eight bit data to be transmitted serially
 	#944 datain=8'b10111001; 
 	#944 datain=8'b11000011;
 	#944 datain=8'b11001100;
